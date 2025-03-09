46c7943d401918dba667aa15ba364466c97b59ec
Giovanni Concohá <gconcohacax21@gmail.com>
2025-03-08 23:05:15 -0600

    API conectada correctamente

--- goLight/Program.cs
+++ goLight/Program.cs
@@ -5,6 +5,17 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddControllers();
 
+// lo de cors se hace en el punto de entrada de la API
+builder.Services.AddCors(options => // depues de los controladores
+{
+    options.AddPolicy("AllowAll",
+        policy =>
+        {
+            policy.AllowAnyOrigin()
+                  .AllowAnyMethod()
+                  .AllowAnyHeader();
+        });
+});
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -19,6 +30,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseCors("AllowAll"); // entes de los controladores
 app.MapControllers();
 
 app.Run();
--- goLight/obj/Debug/net8.0/goLight.AssemblyInfo.cs
+++ goLight/obj/Debug/net8.0/goLight.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("goLight")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e3053677912011085a598d59f073f5cf859705c9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34da60879460badbe09b69b99fdfa5b76fa4cc7d")]
 [assembly: System.Reflection.AssemblyProductAttribute("goLight")]
 [assembly: System.Reflection.AssemblyTitleAttribute("goLight")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
